dd03829315dd32a34eb987f4dc25ce944fae365d
Katie Tanner <Katiejen81@gmail.com>
2016-06-05 12:54:56 -0500

    completed subsection on plotting linear regressions

--- Linear Regression in R.R
+++ Linear Regression in R.R
@@ -33,3 +33,27 @@ predict(lm.fit, data.frame(lstat=(c(5,10,15))),
 #prediction intervals for a given value of the predictor
 predict(lm.fit, data.frame(lstat=(c(5,10,15))),
         interval="prediction")
+
+#scatter plot of predictor and response. This can help us
+#check the linearity assumption
+plot(lstat, medv)
+
+#now we can check our regression line
+abline(lm.fit)
+
+#and let's customize this plot with some beautification
+#increased line width
+abline(lm.fit, lwd=3)
+
+#thicker line and red color
+abline(lm.fit, lwd=3, col="red")
+
+#scatter plot with a different color
+plot(lstat, medv, col="red")
+
+#different plotting symbols
+plot(lstat, medv, pch=20)
+plot(lstat, medv, pch="+")
+
+#this tells us all of the plotting symbols available to us
+plot(1:20, 1:20, pch=1:20)
